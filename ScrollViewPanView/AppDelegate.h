b5728c2880695ef5acbd443ae7a5a759e622c666
秦光义 <Kevin@qinguangyidemacbook-pro.local>
2016-01-20 15:21:07 +0800

    Initial Commit

---
+++ ScrollViewPanView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollViewPanView
+//
+//  Created by 秦光义 on 16/1/20.
+//  Copyright © 2016年 QGY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScrollViewPanView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScrollViewPanView
+//
+//  Created by 秦光义 on 16/1/20.
+//  Copyright © 2016年 QGY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
